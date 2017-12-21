110d5b62bc131e4610a56e09b7d1fd594a5b47f6
Oliver Plunkett <otplunkett@gmail.com>
2017-12-21 09:18:07 +0000

    req val token

--- config/main.js
+++ config/main.js
@@ -1,4 +1,5 @@
 module.exports = {  
   'PORT': process.env.PORT || 3002,
-  'MONGODB_URI': process.env.MONGODB_URI || 'mongodb://localhost:27017/wordcraft'
+  'MONGODB_URI': process.env.MONGODB_URI || 'mongodb://localhost:27017/wordcraft',
+  'VALIDATION_TOKEN': process.env.VALIDATION_TOKEN
 }
