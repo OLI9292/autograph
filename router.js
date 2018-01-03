87221e31903e11e4f2a77c2e75e3171c47948615
Oliver Plunkett <otplunkett@gmail.com>
2018-01-03 15:06:17 -0500

    deprecate v1 routes

--- router.js
+++ router.js
@@ -10,33 +10,13 @@ const UserController = require('./controllers/user')
 module.exports = (app) => {  
   const apiRoutes = express.Router()
   
-  // USER ROUTES
+  apiRoutes.post('/user/create', UserController.create)
   apiRoutes.get('/user', UserController.read)
   apiRoutes.get('/user/:id', UserController.read)
-  apiRoutes.delete('/user', UserController.delete)
-  apiRoutes.patch('/user', UserController.update)
-  apiRoutes.patch('/user/:id', UserController.update2)
-  apiRoutes.post('/user/create', UserController.create)
   apiRoutes.post('/user/login', UserController.login)
-  apiRoutes.post('/user/:id/lesson', LessonController.create)
-
-  // CLASS ROUTES
-  apiRoutes.get('/class', ClassController.read)
-  apiRoutes.get('/class/:id', ClassController.read)
-  apiRoutes.get('/class/:id/students', ClassController.readStudents)
-  apiRoutes.delete('/class/:id', ClassController.delete)
-  apiRoutes.post('/class', ClassController.create)
-  apiRoutes.post('/class/:id', ClassController.join)
-
-  // LESSON ROUTES
-  apiRoutes.post('/lesson', LessonController.create)
-  apiRoutes.get('/lesson', LessonController.read)
-  apiRoutes.get('/lesson/:id', LessonController.read)
-  apiRoutes.patch('/lesson/:id', LessonController.update)
-  apiRoutes.delete('/lesson/:id', LessonController.delete)
+  apiRoutes.patch('/user', UserController.update)
 
   // V2
-
   apiRoutes.post('/v2/login', LoginController.login)  
 
   // CLASS ROUTES
