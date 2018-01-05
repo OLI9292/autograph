68b6f71c676df2d62218dcbc4b957a01bc5a0e19
Oliver Plunkett <otplunkett@gmail.com>
2018-01-05 09:07:53 -0500

    circle yml

--- databases/question/query.js
+++ databases/question/query.js
@@ -2,18 +2,12 @@ const { Pool } = require('pg')
 
 const CONFIG = require('../../config/main')
 
-let pg
-
-if (process.env.NODE_ENV === 'production') {
-  pg = new Pool({
-    host: CONFIG.QUESTION_DB_SERVER,
-    port: CONFIG.QUESTION_DB_HOST,
-    user: CONFIG.QUESTION_DB_USERNAME,
-    password: CONFIG.QUESTION_DB_PASSWORD,
-    database: CONFIG.QUESTION_DB_NAME
-  })
-} else {
-  pg = new Pool({})
-}
+const pg = new Pool({
+  host: CONFIG.QUESTION_DB_SERVER,
+  port: CONFIG.QUESTION_DB_HOST,
+  user: CONFIG.QUESTION_DB_USERNAME,
+  password: CONFIG.QUESTION_DB_PASSWORD,
+  database: CONFIG.QUESTION_DB_NAME
+});
   
 module.exports = (text, params) => pg.query(text, params)
