d81eeddebe05485f63c5a6dab9cb642ac8614696
Oliver Plunkett <otplunkett@gmail.com>
2018-03-15 11:07:46 -0400

    update factoid model

--- models/factoid.js
+++ models/factoid.js
@@ -6,7 +6,9 @@ const _ = require('underscore')
 var factoidSchema = new Schema({
   value: { type: String, required: true },
   word: { type: String, required: true },
-  secondaryWords: [String],
+  words: [String],
+  excludedWords: [String],
+  lastEditedBy: String,
   level: { type: Number, min: 1, max: 10 },
   citation: { type: String, required: true }
 })
