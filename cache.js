8a0d1e0744387a93f27210472bed2ee73b2d9ae8
Oliver Plunkett <otplunkett@gmail.com>
2018-04-07 10:29:23 -0400

    test

--- cache.js
+++ cache.js
@@ -6,7 +6,7 @@ bluebird.promisifyAll(redis.Multi.prototype);
 let client;
 
 if (process.env.REDISTOGO_URL) {
-  const rtg = require("url").parse(uri);
+  const rtg = require("url").parse(process.env.REDISTOGO_URL);
   client = redis.createClient(rtg.port, rtg.hostname);
   client.auth(rtg.auth.split(":")[1]);
 } else {
