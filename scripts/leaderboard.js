8fd8442386937db0c972525e40889aa04dc0734d
Oliver Plunkett <otplunkett@gmail.com>
2018-04-09 20:51:02 -0400

    fix mem leak

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -5,8 +5,8 @@ const LeaderboardController = require("../controllers/leaderboard");
 
 module.exports.cache = async () => {
   // Cache ranks
-  const ranks = _.flatten(await LeaderboardController.allRanks());
+  const ranks = [] //_.flatten(await LeaderboardController.allRanks());
   console.log({ level: "info", message: `Caching ${ranks.length} ranks.` });  
   const stringified = JSON.stringify({ ranks: ranks });
-  await cache.set("leaderboards", stringified);    
+  //await cache.set("leaderboards", stringified);    
 };
